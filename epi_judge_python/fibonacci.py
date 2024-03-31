934f0bed4f577545e08783739ddbf3ed4b8604e2
Hyung Gyun, Lyou <leon1114@naver.com>
2024-03-31 20:10:13 +0900

    fibonacci

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -3,7 +3,17 @@ from test_framework import generic_test
 
 def fibonacci(n: int) -> int:
     # TODO - you fill in here.
-    return -1
+
+    pp, p = 0,1
+    if n <= 1:
+        return n
+
+    tmp = 2
+    while tmp <= n:
+        pp, p = p, p + pp
+        tmp += 1
+
+    return p
 
 
 if __name__ == '__main__':
